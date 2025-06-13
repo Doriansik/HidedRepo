5f0c969ec6c4f8b82bf9a107b30b242c9a81bbc3
Dorian Wolny <dwolny@edu.cdv.pl>
2025-06-13 13:33:27 +0200

    Poprawki i update menu
    Poprawione obracanie obiektow
    Dodanie wygladu do tla i obiekty do klikniecia zamiast zwyklych przyciskow

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -3,17 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void OnStartGame()
+   public static MenuManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene("RoomOne");
     }
 
-    public void OnCreditsScene()
+    public void CreditsScene()
     {
         SceneManager.LoadScene("CreditsMenu");
     }
 
-    public void OnQuitGame()
+    public void QuitGame()
     {
         Application.Quit();
         Debug.Log("das");
