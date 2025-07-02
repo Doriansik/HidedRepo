27be398fa21277dc413a52d45f75792aed689f20
Frisk Dreemurr <friskdreemurrnp@gmail.com>
2025-07-02 19:31:24 +0200

    color&ninja fruit WiP

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -13,6 +13,14 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference BallClick { get; private set; }
     [field: Header("PuzzleSlide")]
     [field: SerializeField] public EventReference PuzzleSlide { get; private set; }
+    [field: Header("ColorPuzzle1")]
+    [field: SerializeField] public EventReference ColorPuzzle1 { get; private set; }
+    [field: Header("ColorPuzzle2")]
+    [field: SerializeField] public EventReference ColorPuzzle2 { get; private set; }
+    [field: Header("ColorPuzzle3")]
+    [field: SerializeField] public EventReference ColorPuzzle3 { get; private set; }
+    [field: Header("ColorPuzzle4")]
+    [field: SerializeField] public EventReference ColorPuzzle4 { get; private set; }
 
 
     public static FMODEvents instance { get; private set; }
