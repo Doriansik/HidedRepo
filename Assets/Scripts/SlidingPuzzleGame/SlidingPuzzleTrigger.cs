76a6e7ebdd79bcc65d9b171ec464918fbb909977
Dorian Wolny <dwolny@edu.cdv.pl>
2025-07-04 00:23:51 +0200

    Zmiany
    zmienilem ikone
    dodalem materialy juz takie normalne na drzwi
    dalem przycisk exit na slidingpuzzle

--- Assets/Scripts/SlidingPuzzleGame/SlidingPuzzleTrigger.cs
+++ Assets/Scripts/SlidingPuzzleGame/SlidingPuzzleTrigger.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class SlidingPuzzleTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject exitSlidingPuzzleButton;
     private void OnMouseDown()
     {
         SlidingPuzzleGameManager.Instance.ChangeCameraToPuzzle();
-        Debug.Log("SlidingPuzzle");
+        exitSlidingPuzzleButton.SetActive(true);
     }
 }
