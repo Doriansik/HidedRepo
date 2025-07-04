b0b070ed28afba240c0243a8cb3b9801300f0175
Dorian <dorianmarekwolny@wp.pl>
2025-07-04 09:29:55 +0200

    FinalGame

--- Assets/Scripts/SlidingPuzzleGame/SlidingPuzzleTrigger.cs
+++ Assets/Scripts/SlidingPuzzleGame/SlidingPuzzleTrigger.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class SlidingPuzzleTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject exitSlidingPuzzleButton;
+    [SerializeField] private GameObject completeSlidingPuzzleButton;
     private void OnMouseDown()
     {
         SlidingPuzzleGameManager.Instance.ChangeCameraToPuzzle();
         exitSlidingPuzzleButton.SetActive(true);
+        completeSlidingPuzzleButton.SetActive(true);
     }
 }
